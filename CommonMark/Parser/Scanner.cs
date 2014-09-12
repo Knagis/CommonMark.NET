06e0ea0fade3d88b4525ee615dc52603f2b1021a
Knagis <Knagis@users.noreply.github.com>
2014-09-12 22:32:36 +0300

    scan_hrule rewritten to remove Regex. 10% improvement for spec.txt

--- CommonMark/Parser/Scanner.cs
+++ CommonMark/Parser/Scanner.cs
@@ -39,12 +39,15 @@ namespace CommonMark.Parser
         // Note - Initial testing showed that there is no benefit of separating the parts of the expression into multiple expressions
         // so the focus for this regex should be to converting parts of the expression completely to C# code.
         private static readonly Regex htmlTagRegex = new Regex("(" + opentag + ")|(" + processinginstruction + ")|(" + htmlcomment + ")|(" + cdata + ")|(" + declaration + ")", useCompilation);
-        //private const string htmltag = "(" + opentag + ")|(" + closetag + ")|(" + htmlcomment + ")|(" + processinginstruction + ")|(" + declaration + ")|(" + cdata + ")";
-        //private static readonly Regex htmltagregex = new Regex(htmltag, useCompilation);
 
         // this Regex had significant impact to performance for some typical documents so it was replaced with custom implementation
         //private const string scheme = "coap|doi|javascript|aaa|aaas|about|acap|cap|cid|crid|data|dav|dict|dns|file|ftp|geo|go|gopher|h323|http|https|iax|icap|im|imap|info|ipp|iris|iris.beep|iris.xpc|iris.xpcs|iris.lwz|ldap|mailto|mid|msrp|msrps|mtqp|mupdate|news|nfs|ni|nih|nntp|opaquelocktoken|pop|pres|rtsp|service|session|shttp|sieve|sip|sips|sms|snmp|soap.beep|soap.beeps|tag|tel|telnet|tftp|thismessage|tn3270|tip|tv|urn|vemmi|ws|wss|xcon|xcon-userid|xmlrpc.beep|xmlrpc.beeps|xmpp|z39.50r|z39.50s|adiumxtra|afp|afs|aim|apt|attachment|aw|beshare|bitcoin|bolo|callto|chrome|chrome-extension|com-eventbrite-attendee|content|cvs|dlna-playsingle|dlna-playcontainer|dtn|dvb|ed2k|facetime|feed|finger|fish|gg|git|gizmoproject|gtalk|hcp|icon|ipn|irc|irc6|ircs|itms|jar|jms|keyparc|lastfm|ldaps|magnet|maps|market|message|mms|ms-help|msnim|mumble|mvn|notes|oid|palm|paparazzi|platform|proxy|psyc|query|res|resource|rmi|rsync|rtmp|secondlife|sftp|sgn|skype|smb|soldat|spotify|ssh|steam|svn|teamspeak|things|udp|unreal|ut2004|ventrilo|view-source|webcal|wtai|wyciwyg|xfire|xri|ymsgr";
         //private static readonly Regex autolink_uri = new Regex("(" + scheme + "):[^<>\\x00-\\x20]*>", useCompilation | RegexOptions.IgnoreCase);
+
+        // these Regex-es were called for every line and resulted in 10% of spec.txt conversion at the time of conversion.
+        //private static readonly Regex hrule1 = new Regex(@"^([\*][ ]*){3,}[\s]*$", useCompilation);
+        //private static readonly Regex hrule2 = new Regex(@"^([_][ ]*){3,}[\s]*$", useCompilation);
+        //private static readonly Regex hrule3 = new Regex(@"^([-][ ]*){3,}[\s]*$", useCompilation);
 
         private static readonly Regex autolink_email = new Regex("^[a-zA-Z0-9.!#$%&'\\*+/=?^_`{|}~-]+[@][a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?([.][a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*[>]", useCompilation);
         private static readonly Regex html_block_tag1 = new Regex("^</" + blocktagname + "(\\s|>)", useCompilation | RegexOptions.IgnoreCase);
@@ -53,9 +56,6 @@ namespace CommonMark.Parser
         private static readonly Regex spacechars = new Regex(@"^[\s]*", useCompilation);
         private static readonly Regex setext_header_line1 = new Regex("^[=]+[ ]*$", useCompilation);
         private static readonly Regex setext_header_line2 = new Regex("^[-]+[ ]*$", useCompilation);
-        private static readonly Regex hrule1 = new Regex(@"^([\*][ ]*){3,}[\s]*$", useCompilation);
-        private static readonly Regex hrule2 = new Regex(@"^([_][ ]*){3,}[\s]*$", useCompilation);
-        private static readonly Regex hrule3 = new Regex(@"^([-][ ]*){3,}[\s]*$", useCompilation);
         private static readonly Regex link_url1 = new Regex("^[ \\n]*[<]([^<>\\n\\x00]|(" + escaped_char + ")|[\\\\])*[>]", useCompilation);
         private static readonly Regex link_url2 = new Regex("^[ \\n]*((" + reg_char + ")+|(" + escaped_char + ")|(" + in_parens_nosp + "))*", useCompilation);
         private static readonly Regex link_title1 = new Regex("^[\"]((" + escaped_char + ")|[^\"\\x00])*[\"]", useCompilation);
@@ -285,7 +285,39 @@ namespace CommonMark.Parser
         /// </summary>
         public static int scan_hrule(string s, int pos)
         {
-            return MatchRegex(s, pos, hrule1, hrule2, hrule3);
+            // @"^([\*][ ]*){3,}[\s]*$",
+            // @"^([_][ ]*){3,}[\s]*$",
+            // @"^([-][ ]*){3,}[\s]*$",
+
+            int count = 0;
+            char c;
+            char x = '\0';
+            var ipos = pos;
+            while (ipos < s.Length)
+            {
+                c = s[ipos++];
+
+                if (c == ' ' || c == '\n')
+                    continue;
+                if (count == 0)
+                {
+                    if (c == '*' || c == '_' || c == '-')
+                        x = c;
+                    else
+                        return 0;
+                    
+                    count = 1;
+                }
+                else if (c == x)
+                    count ++;
+                else
+                    return 0;
+            }
+
+            if (count < 3)
+                return 0;
+
+            return s.Length - pos;
         }
 
         /// <summary>
