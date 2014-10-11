d5ec8fe917400b61d0fc2b395a5b09cacb07b638
Knagis <Knagis@users.noreply.github.com>
2014-10-11 22:56:22 +0300

    Added test for the </h1> bugfix.

---
+++ CommonMark.Tests/HtmlTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonMark.Tests
+{
+    [TestClass]
+    public class HtmlTests
+    {
+        [TestMethod]
+        [TestCategory("Inlines - Raw HTML")]
+        public void HtmlTagsWithDigits()
+        {
+            // Arrange
+            var commonMark = Helpers.Normalize("foo <h1>asd</h1> bar");
+            var expected = Helpers.Normalize("<p>foo <h1>asd</h1> bar</p>");
+            Helpers.LogValue("CommonMark", commonMark);
+            Helpers.LogValue("Expected", expected);
+
+            // Act
+            var actual = CommonMarkConverter.Convert(commonMark);
+
+            // Assert
+            Helpers.LogValue("Actual", actual);
+            Assert.AreEqual(Helpers.Tidy(expected), Helpers.Tidy(actual));
+        }
+    }
+}
