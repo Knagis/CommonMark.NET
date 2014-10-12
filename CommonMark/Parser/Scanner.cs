0a5f94455340d6b71238e695ecef3ba482199a12
Knagis <Knagis@users.noreply.github.com>
2014-10-12 11:51:04 +0300

    Removed duplicate tagname.

--- CommonMark/Parser/Scanner.cs
+++ CommonMark/Parser/Scanner.cs
@@ -14,7 +14,7 @@ namespace CommonMark.Parser
         /// List of valid schemes of an URL. The array must be sorted.
         /// </summary>
         private static readonly string[] schemeArray = new[] { "AAA", "AAAS", "ABOUT", "ACAP", "ADIUMXTRA", "AFP", "AFS", "AIM", "APT", "ATTACHMENT", "AW", "BESHARE", "BITCOIN", "BOLO", "CALLTO", "CAP", "CHROME", "CHROME-EXTENSION", "CID", "COAP", "COM-EVENTBRITE-ATTENDEE", "CONTENT", "CRID", "CVS", "DATA", "DAV", "DICT", "DLNA-PLAYCONTAINER", "DLNA-PLAYSINGLE", "DNS", "DOI", "DTN", "DVB", "ED2K", "FACETIME", "FEED", "FILE", "FINGER", "FISH", "FTP", "GEO", "GG", "GIT", "GIZMOPROJECT", "GO", "GOPHER", "GTALK", "H323", "HCP", "HTTP", "HTTPS", "IAX", "ICAP", "ICON", "IM", "IMAP", "INFO", "IPN", "IPP", "IRC", "IRC6", "IRCS", "IRIS", "IRIS.BEEP", "IRIS.LWZ", "IRIS.XPC", "IRIS.XPCS", "ITMS", "JAR", "JAVASCRIPT", "JMS", "KEYPARC", "LASTFM", "LDAP", "LDAPS", "MAGNET", "MAILTO", "MAPS", "MARKET", "MESSAGE", "MID", "MMS", "MS-HELP", "MSNIM", "MSRP", "MSRPS", "MTQP", "MUMBLE", "MUPDATE", "MVN", "NEWS", "NFS", "NI", "NIH", "NNTP", "NOTES", "OID", "OPAQUELOCKTOKEN", "PALM", "PAPARAZZI", "PLATFORM", "POP", "PRES", "PROXY", "PSYC", "QUERY", "RES", "RESOURCE", "RMI", "RSYNC", "RTMP", "RTSP", "SECONDLIFE", "SERVICE", "SESSION", "SFTP", "SGN", "SHTTP", "SIEVE", "SIP", "SIPS", "SKYPE", "SMB", "SMS", "SNMP", "SOAP.BEEP", "SOAP.BEEPS", "SOLDAT", "SPOTIFY", "SSH", "STEAM", "SVN", "TAG", "TEAMSPEAK", "TEL", "TELNET", "TFTP", "THINGS", "THISMESSAGE", "TIP", "TN3270", "TV", "UDP", "UNREAL", "URN", "UT2004", "VEMMI", "VENTRILO", "VIEW-SOURCE", "WEBCAL", "WS", "WSS", "WTAI", "WYCIWYG", "XCON", "XCON-USERID", "XFIRE", "XMLRPC.BEEP", "XMLRPC.BEEPS", "XMPP", "XRI", "YMSGR", "Z39.50R", "Z39.50S" };
-        private static readonly string[] blockTagNames = new[] { "ARTICLE", "ASIDE", "BLOCKQUOTE", "BODY", "BUTTON", "CANVAS", "CAPTION", "COL", "COLGROUP", "DD", "DIV", "DL", "DT", "EMBED", "FIELDSET", "FIGCAPTION", "FIGURE", "FOOTER", "FOOTER", "FORM", "H1", "H2", "H3", "H4", "H5", "H6", "HEADER", "HGROUP", "HR", "IFRAME", "LI", "MAP", "OBJECT", "OL", "OUTPUT", "P", "PRE", "PROGRESS", "SCRIPT", "SECTION", "STYLE", "TABLE", "TBODY", "TD", "TEXTAREA", "TFOOT", "TH", "THEAD", "TR", "UL", "VIDEO" };
+        private static readonly string[] blockTagNames = new[] { "ARTICLE", "ASIDE", "BLOCKQUOTE", "BODY", "BUTTON", "CANVAS", "CAPTION", "COL", "COLGROUP", "DD", "DIV", "DL", "DT", "EMBED", "FIELDSET", "FIGCAPTION", "FIGURE", "FOOTER", "FORM", "H1", "H2", "H3", "H4", "H5", "H6", "HEADER", "HGROUP", "HR", "IFRAME", "LI", "MAP", "OBJECT", "OL", "OUTPUT", "P", "PRE", "PROGRESS", "SCRIPT", "SECTION", "STYLE", "TABLE", "TBODY", "TD", "TEXTAREA", "TFOOT", "TH", "THEAD", "TR", "UL", "VIDEO" };
 
         /// <summary>
         /// Try to match URI autolink after first &lt;, returning number of chars matched.
@@ -154,8 +154,8 @@ namespace CommonMark.Parser
                 nextChar = s[i];
             }
 
-            var scheme = new string(tagname, 0, j).ToUpperInvariant();
-            if (Array.BinarySearch(blockTagNames, scheme, StringComparer.Ordinal) < 0)
+            var tname = new string(tagname, 0, j).ToUpperInvariant();
+            if (Array.BinarySearch(blockTagNames, tname, StringComparer.Ordinal) < 0)
                 return false;
 
             return nextChar == '>' || (!slashAtBeginning && nextChar == '/') || (nextChar == ' ' || nextChar == '\n');
