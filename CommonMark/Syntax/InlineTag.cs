7fd04e78b6153a3663b650307f94c63232ece1f2
Knagis <Knagis@users.noreply.github.com>
2015-01-12 21:54:52 +0200

    Written comments. XML doc warnings: 51 -> 44.

--- CommonMark/Syntax/InlineTag.cs
+++ CommonMark/Syntax/InlineTag.cs
@@ -4,27 +4,68 @@ using System.Text;
 
 namespace CommonMark.Syntax
 {
+    /// <summary>
+    /// Specifies the element type of an <see cref="Inline"/> instance.
+    /// </summary>
     public enum InlineTag
     {
-        /// <remarks>Original: str</remarks>
+        /// <summary>
+        /// Represents a simple literal string content. Uses <see cref="Inline.LiteralContent"/> to specify the data.
+        /// Cannot contain nested elements.
+        /// </summary>
         String = 0,
 
+        /// <summary>
+        /// Represents a soft-break which by default is rendered as a simple newline and thus does not impact
+        /// the display of the resulting HTML code. The <see cref="CommonMarkSettings.RenderSoftLineBreaksAsLineBreaks"/>
+        /// property can be used to override this behavior and render soft-breaks as <c>&lt;br;&gt;</c> HTML
+        /// elements.
+        /// Cannot contain literal content or nested elements.
+        /// </summary>
         SoftBreak,
 
+        /// <summary>
+        /// Represents a line-break which by default is rendered as a <c>&lt;br;&gt;</c> HTML element.
+        /// Cannot contain literal content or nested elements.
+        /// </summary>
         LineBreak,
 
+        /// <summary>
+        /// Represents an inline code element. Uses <see cref="Inline.LiteralContent"/> to specify the data.
+        /// Cannot contain nested elements.
+        /// </summary>
         Code,
 
-        /// <remarks>Original: raw_html</remarks>
+        /// <summary>
+        /// Represents raw HTML code. Uses <see cref="Inline.LiteralContent"/> to specify the data.
+        /// Cannot contain nested elements.
+        /// </summary>
         RawHtml,
 
-        /// <remarks>Original: emph</remarks>
+        /// <summary>
+        /// Represents an emphasis element. Uses <see cref="Inline.FirstChild"/> to specify the contents.
+        /// Cannot contain literal content.
+        /// </summary>
         Emphasis,
 
+        /// <summary>
+        /// Represents a strong emphasis element. Uses <see cref="Inline.FirstChild"/> to specify the contents.
+        /// Cannot contain literal content.
+        /// </summary>
         Strong,
 
+        /// <summary>
+        /// Represents a link element. Uses <see cref="Inline.FirstChild"/> to specify the content (or label).
+        /// Uses <see cref="Inline.TargetUrl"/> to specify the target of the link and 
+        /// <see cref="Inline.LiteralContent"/> to store the title of the link.
+        /// </summary>
         Link,
 
+        /// <summary>
+        /// Represents an image element. Uses <see cref="Inline.FirstChild"/> to specify the label (description).
+        /// Uses <see cref="Inline.TargetUrl"/> to specify the source of the image and 
+        /// <see cref="Inline.LiteralContent"/> to store the title of the image.
+        /// </summary>
         Image,
 
         /// <summary>
