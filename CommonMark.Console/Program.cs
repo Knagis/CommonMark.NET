67bd658a739e9c9487ac8ccff270461a6229d4d0
Knagis <Knagis@users.noreply.github.com>
2015-01-17 23:03:24 +0200

    CommonMark.Console modified to support both redirected and direct STDIN, to support unicode for input and output and added a special mode that works with how runtests.pl passes the data.
    Also added --bench attribute that will perform a simple benchmark and output the time spent and memory used.

--- CommonMark.Console/Program.cs
+++ CommonMark.Console/Program.cs
@@ -8,26 +8,36 @@ namespace CommonMark
 {
     class Program
     {
-        static void PrintUsage()
+        static void PrintUsage(string invalidArg = null)
         {
+            if (invalidArg != null)
+            {
+                Console.WriteLine("Invalid argument: " + invalidArg);
+                Console.WriteLine();
+            }
+
             Console.WriteLine("Usage:   CommonMark.Console [FILE*] [--out FILE]");
             Console.WriteLine("Options: --help, -h    Print usage information");
             Console.WriteLine("         --ast         Print AST instead of HTML");
+            Console.WriteLine("         --bench 20    Execute a benchmark on the given input, optionally");
+            Console.WriteLine("                       specify the number of iterations, default is 20");
+            Console.WriteLine("         --perltest    Changes console input handling for runtests.pl");
             Console.WriteLine("         --version     Print version");
             Console.WriteLine("         --out         The output file; if not specified, stdout is used");
         }
 
         static int Main(string[] args)
         {
+            var sources = new List<System.IO.TextReader>();
+            var ast = false;
+            var benchmark = false;
+            var benchmarkIterations = 20;
+            var target = Console.Out;
+            var runPerlTests = false;
+
             try
             {
-                var argc = args.Length;
-                int i;
-                bool ast = false;
-                List<string> files = new List<string>();
-                string target = null;
-
-                for (i = 0; i < argc; i++)
+                for (var i = 0; i < args.Length; i++)
                 {
                     if (string.Equals(args[i], "--version", StringComparison.OrdinalIgnoreCase))
                     {
@@ -41,68 +51,126 @@ namespace CommonMark
                         PrintUsage();
                         return 0;
                     }
+                    else if (string.Equals(args[i], "--perltest", StringComparison.OrdinalIgnoreCase))
+                    {
+                        runPerlTests = true;
+                    }
                     else if (string.Equals(args[i], "--ast", StringComparison.OrdinalIgnoreCase))
                     {
                         ast = true;
                     }
+                    else if (string.Equals(args[i], "--bench", StringComparison.OrdinalIgnoreCase))
+                    {
+                        benchmark = true;
+                        if (i != args.Length - 1)
+                        {
+                            if (!int.TryParse(args[i + 1], System.Globalization.NumberStyles.Integer, System.Globalization.CultureInfo.InvariantCulture, out benchmarkIterations))
+                                benchmarkIterations = 20;
+                            else
+                                i++;
+                        }
+                    }
                     else if (string.Equals(args[i], "--out", StringComparison.OrdinalIgnoreCase))
                     {
-                        if (i == argc - 1 || args[i + 1].StartsWith("-"))
+                        if (i == args.Length - 1 || args[i + 1].StartsWith("-"))
                         {
                             PrintUsage();
                             return 1;
                         }
 
                         i++;
-                        target = args[i];
+                        target = new System.IO.StreamWriter(args[i]);
                     }
                     else if (args[i].StartsWith("-"))
                     {
-                        PrintUsage();
+                        PrintUsage(args[i]);
                         return 1;
                     }
                     else
                     {
                         // treat as file argument
-                        files.Add(args[i]);
+                        sources.Add(new System.IO.StreamReader(args[i]));
+                    }
+                }
+
+                if (sources.Count == 0)
+                {
+                    if (runPerlTests)
+                    {
+                        Console.InputEncoding = Encoding.UTF8;
+                        Console.OutputEncoding = Encoding.UTF8;
+
+                        // runtests.pl writes directly to STDIN but will not send Ctrl+C and also
+                        // will get confused by the additional information written to STDOUT
+                        var sb = new StringBuilder();
+                        while (Console.In.Peek() != -1)
+                            sb.AppendLine(Console.In.ReadLine());
+
+                        sources.Add(new System.IO.StringReader(sb.ToString()));
+                    }
+                    else if (!Console.IsInputRedirected)
+                    {
+                        Console.InputEncoding = Encoding.Unicode;
+                        Console.OutputEncoding = Encoding.Unicode;
+
+                        Console.WriteLine("Enter the source. Press Enter after the last line and" + Environment.NewLine + "then press Ctrl+C to run parser.");
+                        Console.WriteLine();
+
+                        Console.CancelKeyPress += (s, a) =>
+                        {
+                            a.Cancel = true;
+                            Console.WriteLine("Output:");
+                            Console.WriteLine();
+                        };
+
+                        sources.Add(Console.In);
+                    }
+                    else
+                    {
+                        Console.InputEncoding = Encoding.UTF8;
+                        Console.OutputEncoding = Encoding.UTF8;
+
+                        sources.Add(Console.In);
                     }
                 }
 
                 var settings = new CommonMarkSettings();
                 settings.OutputFormat = ast ? OutputFormat.SyntaxTree : OutputFormat.Html;
 
-                System.IO.TextWriter writer = target == null ? Console.Out : new System.IO.StreamWriter(target);
-
-                try
+                foreach (var source in sources)
                 {
-                    if (files.Count == 0)
+                    if (benchmark)
                     {
-                        // read from stdin
-                        if (!Console.IsInputRedirected)
+                        var data = source.ReadToEnd();
+
+                        var sw = new System.Diagnostics.Stopwatch();
+                        var mem = GC.GetTotalMemory(true);
+                        long mem2 = 0;
+
+                        for (var x = -1; x < benchmarkIterations; x++)
                         {
-                            Console.WriteLine("Only redirected STDIN is supported.");
-                            PrintUsage();
-                            return 2;
+                            if (x == 0)
+                                sw.Start();
+
+                            using (var reader = new System.IO.StringReader(data))
+                            using (var twriter = new System.IO.StringWriter())
+                                CommonMarkConverter.Convert(reader, twriter, settings);
+
+                            if (mem2 == 0)
+                                mem2 = GC.GetTotalMemory(false);
+
+                            GC.Collect();
                         }
 
-                        CommonMarkConverter.Convert(Console.In, writer, settings);
+                        sw.Stop();
+                        target.WriteLine("Time spent: {0:0.#}ms    Approximate memory usage: {1:0.000}MB",
+                            (decimal)sw.ElapsedMilliseconds / benchmarkIterations,
+                            (mem2 - mem) / 1024M / 1024M);
                     }
                     else
                     {
-                        // iterate over input file pointers
-                        for (var g = 0; g < files.Count; g++)
-                        {
-                            using (var reader = new System.IO.StreamReader(files[g]))
-                            {
-                                CommonMarkConverter.Convert(reader, writer, settings);
-                            }
-                        }
+                        CommonMarkConverter.Convert(source, target, settings);
                     }
-                }
-                finally
-                {
-                    if (writer != Console.Out)
-                        writer.Close();
                 }
 
                 return 0;
@@ -111,6 +179,15 @@ namespace CommonMark
             {
                 Console.Error.WriteLine(ex.ToString());
                 return -1;
+            }
+            finally
+            {
+                foreach (var s in sources)
+                    if (s != Console.In)
+                        s.Close();
+
+                if (target != Console.Out)
+                    target.Close();
             }
         }
     }
