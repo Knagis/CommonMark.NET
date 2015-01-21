f81cd9efef902f78d808ce31bb709def4a80aa9f
Knagis <Knagis@users.noreply.github.com>
2015-01-21 22:09:29 +0200

    Helper methods updated. Removed the license note from CommonMarkSharp since there is nothing of importance left from it. The license still applies to Spec.tt file.

--- CommonMark.Tests/Helpers.cs
+++ CommonMark.Tests/Helpers.cs
@@ -1,34 +1,8 @@
-﻿/*
-The MIT License (MIT)
-
-Copyright (c) 2014 Morten Houston Ludvigsen
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace CommonMark.Tests
 {
@@ -46,22 +20,29 @@ namespace CommonMark.Tests
 
         public static void ExecuteTest(string commonMark, string html, CommonMarkSettings settings = null)
         {
+            Helpers.LogValue("CommonMark", Denormalize(commonMark));
+            Helpers.LogValue("Expected", Denormalize(html));
+
             // Arrange
             commonMark = Helpers.Normalize(commonMark);
             html = Helpers.Normalize(html);
-
-            Helpers.LogValue("CommonMark", commonMark);
-            Helpers.LogValue("Expected", html);
 
             // Act
             var actual = CommonMarkConverter.Convert(commonMark, settings);
 
             // Assert
-            Helpers.LogValue("Actual", actual);
+            Helpers.LogValue("Actual", Denormalize(actual));
             Assert.AreEqual(Helpers.Tidy(html), Helpers.Tidy(actual));
         }
 
-        public static string Normalize(string value)
+        private static string Denormalize(string value)
+        {
+            value = value.Replace('\t', '→');
+            value = value.Replace(' ', '␣');
+            return value;
+        }
+
+        private static string Normalize(string value)
         {
             value = value.Replace('→', '\t');
             value = value.Replace('␣', ' ');
@@ -79,44 +60,26 @@ namespace CommonMark.Tests
             // needed to compare UTF-32 characters
             html = html.Normalize(NormalizationForm.FormKD);
             return html;
-
-            ////var result = new StringBuilder();
-            ////var inPre = false;
-            ////foreach (var line in html.Split('\n'))
-            ////{
-            ////    if (Regex.IsMatch(line, @"<pre"))
-            ////        inPre = true;
-            ////    if (Regex.IsMatch(line, @"</pre"))
-            ////        inPre = false;
-            ////    if (inPre)
-            ////        result.AppendLine(line);
-            ////    else
-            ////        result.AppendLine(line.Trim());
-            ////}
-            ////return result.ToString();
-        }
-
-        public static void Log()
-        {
-            Console.WriteLine();
-        }
-
-        public static void Log(string message)
-        {
-            Console.WriteLine(message);
         }
 
         public static void Log(string format, params object[] args)
         {
-            Log(string.Format(format, args));
+            if (args != null && args.Length > 0)
+                Console.WriteLine(format, args);
+            else
+                Console.WriteLine(format);
         }
 
         public static void LogValue(string caption, string text)
         {
-            Log("{0}:", caption);
-            Log();
-            Log(string.Join("\n", text.Replace("\r", "").Split('\n', '\r').Select(s => "    " + s)));
-            Log();
+            Console.Write(caption);
+            Console.WriteLine(":");
+            Console.WriteLine();
+
+            Console.Write("    ");
+            Console.WriteLine(text.Replace("\n", "\n    "));
+
+            Console.WriteLine();
         }
     }
 }
