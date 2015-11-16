bc023971015c2acfabe4cda1b44d7e0f17b71ecc
Dmitry Shechtman <damnian@gmail.com>
2015-11-16 23:10:31 +0200

    Propagate settings to handlers

--- CommonMark/CommonMarkAdditionalFeatures.cs
+++ CommonMark/CommonMarkAdditionalFeatures.cs
@@ -21,11 +21,6 @@ namespace CommonMark
         StrikethroughTilde = 1,
 
         /// <summary>
-        /// The parser will treat the case of reference labels with sensitivitiley.
-        /// </summary>
-        RespectReferenceCase = 2,
-
-        /// <summary>
         /// All additional features are enabled.
         /// </summary>
         All = 0x7FFFFFFF
